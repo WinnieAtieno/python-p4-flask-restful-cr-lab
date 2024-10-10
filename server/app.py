525fa6ada52ce0b0f19c0ca64dfeb5514d768fa6
WinnieAtieno <winnieotieno773@gmail.com>
2024-10-10 23:52:30 +0300

    completed labs

--- server/app.py
+++ server/app.py
@@ -17,11 +17,35 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plant = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(plant,200)
+        return response
+    def post(self):
+        data = request.get_json()
+        name = data.get('name')
+        image = data.get('image')
+        price = data.get('price')
 
-class PlantByID(Resource):
-    pass
+        new_plant = Plant(name=name,image=image,price=price)
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(new_plant.to_dict(),201)
+        return response
         
 
+class PlantByID(Resource):
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            response = make_response(plant.to_dict(),200)
+            return response
+        
+  
+        
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
